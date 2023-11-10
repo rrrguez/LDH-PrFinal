18b4b397c480c7fb84cbf4dcdedc2889dad0e5fd
Adnan Hawari Capa <91922795+Adn086@users.noreply.github.com>
2023-11-10 11:20:19 +0000

    Update Main.java
    Añadidos requisitos de solicitud del nombre del fichero por consola y por argumento

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,31 @@
 package main.java;
 
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        
-    	
-    	Reader leitor = new Reader("bayg29.txt");
-        
-        
+        String filename;
+
+        if (args.length > 0) {
+            filename = args[0];
+        } else {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Caipirinha gaucho, ingreissse el nombre do archivinho: ");
+            filename = scanner.nextLine();
+            scanner.close();
+        }
+
+        Reader leitor = new Reader(filename);
         Grafo G = new Grafo(leitor.getnVertices(), leitor.getVertices(), leitor.getArestas());
 
         int iteracoesGrasp = 2;
         double alfa = 0.1;
-        
+
         MetGrasp grasp = new MetGrasp(G, iteracoesGrasp, alfa);
-        
-        
-        //LACO PARA INICIALIZAR GRASP 10 VEZES COM N(iteracoesGrasp) ITERACOES)
+
+        // LACO PARA INICIALIZAR GRASP 10 VEZES COM N(iteracoesGrasp) ITERACOES)
         for (int i = 0; i < 10; ++i) grasp.run();
-        
     }
-    
+
 }
